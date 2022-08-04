b497523f59e66f4259796fbc1be033a0e1d80f07
Milo Wical <milo.wical@gmail.com>
2022-08-04 10:49:05 -0400

    Took long enough to scaffold...

---
+++ src/Producer/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.CommandLine;
+
+Console.WriteLine("Hello, World!");
+
+var fileOption = new Option<string>(
+            name: "--file",
+            description: "The file to read and display on the console.");
+
+var rootCommand = new RootCommand("Sample app for System.CommandLine");
+rootCommand.AddOption(fileOption);
+
+rootCommand.SetHandler((fileName) => {
+  Console.WriteLine(fileName);
+}, fileOption);
+
+return await rootCommand.InvokeAsync(args);
